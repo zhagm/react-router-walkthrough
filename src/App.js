3786efb67c9833bbc0cada360e994b35fce9b5ba
zhagm <zhagdev@gmail.com>
2020-06-26 10:32:30 -0700

    Starting point

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Nav from "./components/Nav";
 import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import NotFound from "./components/NotFound";
-import HelloWorld from "./components/HelloWorld";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/helloworld/:name?" component={HelloWorld} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+      <Nav />
+      <Home />
     </div>
   );
 }
